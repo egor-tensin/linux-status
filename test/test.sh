98d0dbcc63322cd4952823d75a2ed16594f72141
Egor Tensin <egor@tensin.name>
2026-09-21 22:36:23 +0500

    remove annoying trailing period from repo URLs

--- test/test.sh
+++ test/test.sh
@@ -2,7 +2,7 @@
 
 # Copyright (c) 2021 Egor Tensin <egor@tensin.name>
 # This file is part of the "linux-status" project.
-# For details, see https://github.com/egor-tensin/linux-status.
+# For details, see https://github.com/egor-tensin/linux-status
 # Distributed under the MIT License.
 
 set -o errexit -o nounset -o pipefail
